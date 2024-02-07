6aa50e6bb4249384972d2a4f5a08e23db517b5f2
Stefano Tumino <47145720+Nefasto1@users.noreply.github.com>
2024-02-07 09:14:53 +0100

    Update preprocess.py

--- Exercise 1/solution/preprocess.py
+++ Exercise 1/solution/preprocess.py
@@ -1,16 +1,21 @@
 # Substitute all the spaces inside the results with a tab
-for i in range(1, 10): 
-        with open(f"results/results_thin_{i}.txt", "r") as f:
-                string = f.readlines()
-
-        newstring = [string[0]]
-        for row in string[2:]:
-                tmp = row.split(" ")
-                row = tmp[0] + "\t" + tmp[-1]
-                newstring.append(row)
-
-        with open(f"results/results_thin_preprocessed_{i}.txt", "w") as f:
-                f.writelines(newstring)
+thin_count=11
+for i in range(1, 11): 
+        try:    
+                with open(f"results/results_thin_{i}.txt", "r") as f:
+                        string = f.readlines()
+        
+                newstring = [string[0]]
+                for row in string[2:]:
+                        tmp = row.split(" ")
+                        row = tmp[0] + "\t" + tmp[-1]
+                        newstring.append(row)
+        
+                with open(f"results/results_thin_preprocessed_{i}.txt", "w") as f:
+                        f.writelines(newstring)
+        except:
+                thin_count = i
+                break
 
 # Substitute all the spaces inside the latency results with a tab
 for i in ["core", "socket", "node"]: 
@@ -40,25 +45,29 @@ for i in ["core", "socket", "node"]:
                 f.writelines(newstring)
 
 # Substitute all the spaces inside the results with a tab
+fixed_thin_count=11
 for i in range(1, 11): 
-        with open(f"results/fixed_thin_{i}.txt", "r") as f:
-                string = f.readlines()
-
-        newstring = [string[0]]
-        for row in string[2:]:
-                tmp = row.split(" ")
-                row = tmp[0] + "\t" + tmp[-1]
-                newstring.append(row)
-
-        with open(f"results/fixed_thin_preprocessed_{i}.txt", "w") as f:
-                f.writelines(newstring)
-                
+        try:
+                with open(f"results/fixed_thin_{i}.txt", "r") as f:
+                        string = f.readlines()
+        
+                newstring = [string[0]]
+                for row in string[2:]:
+                        tmp = row.split(" ")
+                        row = tmp[0] + "\t" + tmp[-1]
+                        newstring.append(row)
+        
+                with open(f"results/fixed_thin_preprocessed_{i}.txt", "w") as f:
+                        f.writelines(newstring)
+        except:
+                fixed_thin_count = i
+                break
 
 # merge the results
 import pandas as pd
 
-thin = [pd.read_csv(f"results/results_thin_preprocessed_{i}.txt", sep="\t") for i in range(1, 3)]
-fixed_thin = [pd.read_csv(f"results/fixed_thin_preprocessed_{i}.txt", sep="\t") for i in range(1, 11)]
+thin = [pd.read_csv(f"results/results_thin_preprocessed_{i}.txt", sep="\t") for i in range(1, thin_count)]
+fixed_thin = [pd.read_csv(f"results/fixed_thin_preprocessed_{i}.txt", sep="\t") for i in range(1, fixed_thin_count)]
 
 merged_thin = pd.concat(thin)
 merged_fixed_thin = pd.concat(fixed_thin)
