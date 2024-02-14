09b7e53371ea3ea1f34ff89dfc7e4a1b42a839b3
Stefano Tumino <47145720+Nefasto1@users.noreply.github.com>
2024-02-14 08:06:10 +0100

    Update compile_all.sh

--- Exercise 2/solution/compile_all.sh
+++ Exercise 2/solution/compile_all.sh
@@ -2,6 +2,10 @@
 module load openMPI/4.1.5/gnu
 
 # Compile the .c code
-mpicc src/MPI_b_cast.c -o bin/MPI_b_cast.o
+mpicc src/MPI_b_cast_flat.c -o bin/MPI_b_cast_flat.o
+mpicc src/MPI_b_cast_chain.c -o bin/MPI_b_cast_chain.o
+mpicc src/MPI_b_cast_binary.c -o bin/MPI_b_cast_binary.o
 mpicc -qopenmp src/OpenMP_b_cast.c -o bin/OpenMP_b_cast.o
+
+# Ex 2b
 mpicc -qopenmp src/quicksort.c -o bin/quicksort.o
